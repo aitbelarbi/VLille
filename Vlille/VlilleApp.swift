9b32c079798ed4b9d849ddee84408a6829fdb5f8
maitbelarbi <mohamedamine.aitbelarbi.partner@decathlon.com>
2025-02-13 11:16:56 +0100

    Initial Commit

---
+++ Vlille/VlilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VlilleApp.swift
+//  Vlille
+//
+//  Created by Mohamed Amine AIT BELARBI on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct VlilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
